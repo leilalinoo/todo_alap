bb2dce8f7fc052e7f1118d322e7de2504ab11235
leilalinoo <aleila0921@gmail.com>
2023-09-19 11:51:24 +0200

    id-k torolve

--- adatok.js
+++ adatok.js
@@ -11,48 +11,39 @@ export const TODOLIST = [
 ];
 export const TODOLIST2 = [
   {
-    id: 1,
     tevekenyseg: "szakdolgozat specifikáció elkészítése",
     hatarido: "2023.10.15",
   },
   {
-    id: 2,
     tevekenyseg: "drótváz, funkciólista",
     hatarido: "2023.10.30",
   },
   {
-    id: 3,
     tevekenyseg: "adatbázis tervezés, létrehozás",
     hatarido: "2023.12.11",
   },
   {
-    id: 4,
     tevekenyseg: "backend 80%",
     hatarido: "2024.01.20",
   },
   {
-    id: 5,
     tevekenyseg: "frontend 80%",
     hatarido: "2023.02.11",
   },
   {
-    id: 6,
     tevekenyseg: "OSZTV",
     hatarido: "2023.02.26",
   },
   {
-    id: 7,
     tevekenyseg: "tesztelés, dokumentáció",
     hatarido: "folyamatosan",
   },
 
   {
-    id: 8,
     tevekenyseg: "kész szakdolgozat legalább 99%-os készültséggel",
     hatarido: "2024.03.31",
   },
   {
-    id: 9,
     tevekenyseg: "kész dokumentáció",
     hatarido: "2024.04.15",
   },
